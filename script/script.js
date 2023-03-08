3ebbf5d17944cdf2c15e291d2cbfd7cbcfac5228
Ida Katrine Presttun-Brekken <81631851+Prebre@users.noreply.github.com>
2023-03-08 21:09:30 +0100

    Added JS
    Added javascript to my script.js file
    - Had the nav display only when scrolled beyond a certain point.
    - Added form validation.
    
    I also considered adding other functionalities, but I'll focus on the necessary for now.
    
    I also added a few responsive bits of code.

---
+++ script/script.js
@@ -0,0 +1,45 @@
+/* Show nav on scroll */
+let navigation = document.querySelector("nav");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (
+    document.body.scrollTop > 150 ||
+    document.documentElement.scrollTop > 150
+  ) {
+    navigation.style.display = "block";
+  } else {
+    navigation.style.display = "none";
+  }
+}
+
+/* Form Validation */
+function validateForm() {
+  let fullName = document.forms["contactForm"]["fullname"].value;
+  let mail = document.forms["contactForm"]["mail"].value;
+  const emailRegExp =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+  let subject = document.forms["contactForm"]["subject"].value;
+  let message = document.forms["contactForm"]["message"].value;
+  if (fullName < 5) {
+    alert("Name must contain more than 5 symbols");
+    return false;
+  }
+  if (mail.indexOf("@") == -1 || mail.length < 5) {
+    alert("Please fill in a valid email address");
+    return false;
+  }
+  if (subject < 15) {
+    alert("Subject must contain more than 15 symbols");
+    return false;
+  }
+  if (message < 20) {
+    alert("Message must contain more than 20 symbols");
+    return false;
+  } else {
+    return true;
+  }
+}
